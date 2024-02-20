fc533765beba6551e5ebeaece44cf4f59b92405e
bidmak <bidmak4sure@yahoo.com>
2024-02-20 03:38:12 +0000

    Reorganised files and updated README

---
+++ Evolving-Strings/README.md
@@ -0,0 +1,19 @@
+# Evolving Strings Problem
+
+This project aims to implement GA, Hill climbing, and Random search algorithm to evolve a given target string: "Hi. My name is Samuel Makanjuola, and this is a simple demonstration of Genetic Algorithm. Thanks!!!!". The target string consists of 100 characters, making it a challenging search-based problem. Feel free to make appropriate changes as needed, such as modifying the target string or adjusting the GA parameters, to suit your requirements.
+
+## GA Results
+
+After 51 iterations, the GA successfully evolved the target string:
+
+- **Solution**: "Hi. My name is Samuel Makanjuola, and this is a simple demonstration of Genetic Algorithm. Thanks!!!!"
+- **Fitness Score**: 100/100
+- **Time Taken**: 60.89 seconds
+
+## Hill Climbing Results
+
+After 745 iterations, Hill climber successfully evolved the target string:
+
+- **Solution**: "Hi. My name is Samuel Makanjuola, and this is a simple demonstration of Genetic Algorithm. Thanks!!!!"
+- **Fitness Score**: 100/100
+- **Time Taken**: 16.17 seconds
---
+++ Evolving-Strings/hill_es.py
@@ -0,0 +1,45 @@
+from ga_es import mutate_solution, generate_solution, fitness, TARGET, LEN_TAR
+import time
+
+
+def get_neighbours(solution, n=1000):
+    neighbours = list()
+    for _ in range(n):
+        neighbours.append(mutate_solution(solution, rate=0.05))
+    return neighbours
+
+
+generation = 0
+best_solution = generate_solution(LEN_TAR)
+best_score = fitness(target=TARGET, solution=best_solution)
+
+print("----- Hill Climbing Algorithm On Evolving Strings -----")
+start_time = time.time()
+
+if best_score == LEN_TAR:
+    print(f"Iteration: {generation}, {best_solution}, score: {best_score}/{LEN_TAR}")
+
+while best_score < LEN_TAR:
+    generation += 1
+    neighbours = get_neighbours(best_solution)
+    for neighbour in neighbours:
+        neighbour_score = fitness(target=TARGET, solution=neighbour)
+
+        # First choice hill-climbing variant
+        if neighbour_score > best_score:
+            best_solution = neighbour
+            best_score = neighbour_score
+            print(f"Iteration: {generation}, Fitness score: {best_score} of {LEN_TAR}")
+            break
+
+    if best_score == LEN_TAR:
+        print(f"\n-----------  Hill Climbing Solution Results -----------\n")
+
+        print(
+            f"Solution: {best_solution} \nFitness score: {best_score}/{LEN_TAR} \nIteration: {generation}"
+        )
+        break
+
+end_time = time.time()
+print(f"Time taken: {end_time - start_time} seconds")
+print(f"\n------------ End of Hill Climbing Operation ------------\n")
